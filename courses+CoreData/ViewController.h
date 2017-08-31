b4f23071ee124da02be651fe01317bc8e5094c7d
keepfocused <admin@mac-admin.local>
2017-08-31 10:28:09 +0300

    Initial Commit

---
+++ courses+CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  courses+CoreData
+//
+//  Created by Admin on 31.08.17.
+//  Copyright © 2017 Galiev Danil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
